3ff0417657cda5f0c62a74cbe817783250bfee57
dimashidayatulloh <dimaskalem07@gmail.com>
2024-10-13 19:41:09 +0700

    OOP #12 - Constant

---
+++ oop/constant.php
@@ -0,0 +1,36 @@
+<?php
+/*
+define('NAMA', 'Dimas H');
+echo NAMA;
+
+echo "<br>";
+
+const UMUR = 32;
+echo UMUR;
+*/
+
+/*
+class Coba
+{
+    const NAMA = "Dimas";
+}
+
+echo Coba::NAMA;
+*/
+
+// echo __FILE__;
+
+// function coba()
+// {
+//     return __FUNCTION__;
+// }
+
+// echo coba();
+
+class Coba
+{
+    public $kelas = __CLASS__;
+}
+
+$obj = new Coba();
+echo $obj->kelas;
