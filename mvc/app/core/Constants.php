35ea547061e89c2116d0af3bc3819e1ce34d989d
dimashidayatulloh <dimaskalem07@gmail.com>
2024-10-20 18:19:27 +0700

    MVC Assets

---
+++ mvc/app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php
+
+define('BASEURL', 'http://localhost:8080/prakweb_2024_b_223040020/mvc/public/');
--- mvc/app/views/about/index.php
+++ mvc/app/views/about/index.php
@@ -1,2 +1,5 @@
-<h1>About Me</h1>
-<p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?></p>
+<div class="container mt-4">
+    <h1>About Me</h1>
+    <img src="<?= BASEURL; ?>/img/profile.jpg" alt="profile" width="200" height="200" class="rounded-circle shadow">
+    <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?></p>
+</div>
